cfe1fffd95eebe4f6dea2b3957ffe528c8668eb3
Social Exchange <Social Exchange@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-26 22:10:29 +0000

    Update 1.1
    git-svn-id: https://plugins.svn.wordpress.org/social-exchange-plugin/trunk@762645 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-exchange.php
+++ social-exchange.php
@@ -1,1 +1,15 @@
-<?php	/*		Plugin Name: Social Exchange		Plugin URI: http://www.socialexchangeplugin.com/		Description: Social exchange plugin for wordpress		Version: 1.0		Author: Social Exchange Plugin team		Author URI: http://www.socialexchangeplugin.com/		License: GPL	*/ 	$sxpath =  plugin_dir_path(__FILE__ );;	require_once "inc/config.php";	require_once "inc/common.php";	require_once "inc/render.php";?>
+<?php
+	/*
+		Plugin Name: Social Exchange
+		Plugin URI: http://www.socialexchangeplugin.com/
+		Description: Social exchange plugin for wordpress
+		Version: 1.1
+		Author: Social Exchange Plugin team
+		Author URI: http://www.socialexchangeplugin.com/
+		License: GPL
+	*/ 
+	$sxpath =  plugin_dir_path(__FILE__ );;
+	require_once "inc/config.php";
+	require_once "inc/common.php";
+	require_once "inc/render.php";
+?>
