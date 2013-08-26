03590168ef742a35674ce8132c3e6f55e6ea1f63
Social Exchange <Social Exchange@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-26 21:23:16 +0000

    “Tagging version 1.0"
    git-svn-id: https://plugins.svn.wordpress.org/social-exchange-plugin/tags@762610 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.0/inc/config.php
@@ -0,0 +1,1 @@
+<?php	function sx($name){		$return = '';		switch($name){		case 'api_url':			$return = 'http://morefansforyou.com/sx/';		break;  		case 'plugin_url';			$return = plugins_url()."/social-exchange/";		break;		default:			$return =  '';		break; 		}				return $return;	}?>
